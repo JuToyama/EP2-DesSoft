6cfd6f64499c9b55d32152cada959ceda55e55a9
Julia <juliahtc@al.insper.edu.br>
2021-05-06 21:50:32 -0300

    Função que cria baralho

---
+++ cria_baralho.py
@@ -0,0 +1,9 @@
+def cria_baralho():
+    lista=['A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦']
+    lista.sort()
+    lista_embara=[]
+    lista_embara.append(lista[0])
+    lista_embara.append(lista[1])
+    lista_embara.append(lista[2])
+    lista_embara.append(lista[3])   
+    return lista
