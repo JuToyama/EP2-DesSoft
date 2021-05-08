183d95cd8c8c1aa5f788885743d1c21377808a07
Julia <juliahtc@al.insper.edu.br>
2021-05-08 20:52:40 -0300

    Função que extrai valor

---
+++ extrai_valor.py
@@ -0,0 +1,28 @@
+def extrai_valor(carta):
+    if carta=='A♦' or carta=='A♥' or carta=='A♣' or carta=='A♠':
+        return 'A'
+    if carta=='2♦' or carta=='2♥' or carta=='2♣' or carta=='2♠':
+        return '2'
+    if carta=='3♦' or carta=='3♥' or carta=='3♣' or carta=='3♠':
+        return '3'
+    if carta=='4♦' or carta=='4♥' or carta=='4♣' or carta=='4♠':
+        return '4'
+    if carta=='5♦' or carta=='5♥' or carta=='5♣' or carta=='5♠':
+        return '5'
+    if carta=='6♦' or carta=='6♥' or carta=='6♣' or carta=='6♠':
+        return '6'
+    if carta=='7♦' or carta=='7♥' or carta=='7♣' or carta=='7♠':
+        return '7'
+    if carta=='8♦' or carta=='8♥' or carta=='8♣' or carta=='8♠':
+        return '8'
+    if carta=='9♦' or carta=='9♥' or carta=='9♣' or carta=='9♠':
+        return '9'
+    if carta=='10♦' or carta=='10♥' or carta=='10♣' or carta=='10♠':
+        return '10'
+    if carta=='J♦' or carta=='J♥' or carta=='J♣' or carta=='J♠':
+        return 'J'
+    if carta=='Q♦' or carta=='Q♥' or carta=='Q♣' or carta=='Q♠':
+        return 'Q'
+    if carta=='K♦' or carta=='K♥' or carta=='K♣' or carta=='K♠':
+        return 'K'
+    
